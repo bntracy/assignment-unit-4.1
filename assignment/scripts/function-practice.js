afbd65947cd4e281fa0f5d7765d6dc4da17c4e79
Beth Tracy <bntracy@gmail.com>
2024-01-04 14:44:52 -0600

    credit Edabit

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -124,7 +124,7 @@ console.log('Testing allPositive with [0, -5]', allPositive([0, -5]));
 // Create a function that takes a number as an argument. Add up all the numbers
 // from 1 to the number you passed to the function. For example, if the input is
 // 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10. Expect any
-// positive number between 1 and 1000.
+// positive number between 1 and 1000. (from Edabit)
 function addUp(num) {
   let sum = 0;
   for (let i = 1; i <= num; i++) {
